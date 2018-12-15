afe7d0d33e92cac3db5be671207e62879e0f66e8
Karam Kabbara <contact@karam.io>
2018-12-15 22:42:25 +0000

    Setting windows line break style instead of unix. Oops.

--- app/Controllers/Http/EpisodeController.js
+++ app/Controllers/Http/EpisodeController.js
@@ -1,81 +1,81 @@
-'use strict'
+"use strict";
 
-const Podcast = use('App/Models/Podcast')
-const Episode = use('App/Models/Episode')
-const { validateAll } = use('Validator')
-const Helpers = use('Helpers')
-const Event = use('Event')
+const Podcast = use("App/Models/Podcast");
+const Episode = use("App/Models/Episode");
+const { validateAll } = use("Validator");
+const Helpers = use("Helpers");
+const Event = use("Event");
 
 class EpisodeController {
-    async create({ view, params }) {
-        const podcast = await Podcast.findByOrFail('slug', params.slug)
+  async create({ view, params }) {
+    const podcast = await Podcast.findByOrFail("slug", params.slug);
 
-        return view.render('episodes.create', { podcast: podcast.toJSON() })
+    return view.render("episodes.create", { podcast: podcast.toJSON() });
+  }
+
+  async store({ request, response, auth, session }) {
+    const validation = await validateAll(request.all(), {
+      podcast_id: "required",
+      title: "required",
+      summary: "required"
+    });
+
+    if (validation.fails()) {
+      session.withErrors(validation.messages()).flashAll();
+
+      return response.redirect("back");
     }
 
-    async store({ request, response, auth, session }) {
-        const validation = await validateAll(request.all(), {
-            podcast_id: 'required',
-            title: 'required',
-            summary: 'required'
-        })
+    const podcast = await Podcast.query().where("id", request.input("podcast_id")).withCount("episodes").first();
 
-        if (validation.fails()) {
-            session.withErrors(validation.messages()).flashAll()
+    const audio = request.file("audio", { type: ["mp3"] });
+    await audio.move(Helpers.publicPath("uploads/audios"), {
+      name: `${podcast.slug}_${podcast.episodes_count + 1}.${audio.subtype}`
+    });
 
-            return response.redirect('back')
+    if (!audio.moved()) {
+      session.flash({
+        notification: {
+          type: "danger",
+          message: audio.error().message
         }
+      });
 
-        const podcast = await Podcast.query().where('id', request.input('podcast_id')).withCount('episodes').first()
-
-        const audio = request.file('audio', { type: ['mp3'] })
-        await audio.move(Helpers.publicPath('uploads/audios'), {
-            name: `${podcast.slug}_${podcast.episodes_count + 1}.${audio.subtype}`
-        })
-
-        if (!audio.moved()) {
-            session.flash({
-                notification: {
-                    type: 'danger',
-                    message: audio.error().message
-                }
-            })
-
-            response.redirect('back')
-        }
-
-        const episode = new Episode()
-        episode.title = request.input('title')
-        episode.summary = request.input('summary')
-        episode.user_id = auth.user.id
-        episode.audio = `uploads/audios/${audio.fileName}`
-
-        await podcast.episodes().save(episode)
-
-        const subscribers = await podcast.subscribers().fetch()
-
-        if (subscribers.toJSON().length > 0) {
-            Event.fire('new::episode', {
-                podcast,
-                subscribers: subscribers.toJSON()
-            })
-        }
-
-        session.flash({
-            notification: {
-                type: 'success',
-                message: 'Episode uploaded!'
-            }
-        })
-
-        return response.route('myPodcast')
+      response.redirect("back");
     }
 
-    async download ({params, response}) {
-        const episode = await Episode.findOrFail(params.id)
+    const episode = new Episode();
+    episode.title = request.input("title");
+    episode.summary = request.input("summary");
+    episode.user_id = auth.user.id;
+    episode.audio = `uploads/audios/${audio.fileName}`;
 
-        return response.attachment(Helpers.publicPath(episode.audio))
+    await podcast.episodes().save(episode);
+
+    const subscribers = await podcast.subscribers().fetch();
+
+    if (subscribers.toJSON().length > 0) {
+      Event.fire("new::episode", {
+        podcast,
+        subscribers: subscribers.toJSON()
+      });
     }
+
+    session.flash({
+      notification: {
+        type: "success",
+        message: "Episode uploaded!"
+      }
+    });
+
+    return response.route("myPodcast");
+  }
+
+  async download({ params, response }) {
+    const episode = await Episode.findOrFail(params.id);
+
+    return response.attachment(Helpers.publicPath(episode.audio));
+  }
 }
 
-module.exports = EpisodeController
+module.exports = EpisodeController;
